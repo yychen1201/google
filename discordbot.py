ea7f8645265404029f49c72e5db88b88822fceb6
yychen1201 <113757435+yychen1201@users.noreply.github.com>
2022-10-09 17:59:50 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,3 +1,5 @@
+
+
 from distutils.log import error
 import discord
 from discord.ext import commands
@@ -38,6 +40,109 @@ client = commands.Bot(command_prefix = "!",intents=intent)
 @client.event
 async def on_ready():
     print('成功登入')
+
+#添加身分組
+@client.event
+async def on_raw_reaction_add(payload):
+    if str(payload.message_id) == rolemessageid:
+        guild = client.get_guild(int(guildid))
+        result = service.spreadsheets().values().get(
+        spreadsheetId=SpreadsheetId, range=reactRange).execute()
+        values = result.get('values', [])
+        if not values:
+            return
+        for row in values:
+            if payload.emoji.name == row[0]:
+                role = discord.utils.get(guild.roles, name=row[1])
+                if not role == None:
+                    await payload.member.add_roles(role)
+
+#移除身分組
+@client.event
+async def on_raw_reaction_remove(payload):
+    if str(payload.message_id) == rolemessageid:
+        guild = client.get_guild(int(guildid))
+        result = service.spreadsheets().values().get(
+        spreadsheetId=SpreadsheetId, range=reactRange).execute()
+        values = result.get('values', [])
+        if not values:
+            return
+        for row in values:
+            if payload.emoji.name == row[0]:
+                role = discord.utils.get(guild.roles, name=row[1])
+                member = discord.utils.get(guild.members, id=payload.user_id)
+                if not role == None:
+                    await member.remove_roles(role)
+
+
+
+# 收到訊息時呼叫
+@client.event
+async def on_message(message):
+    
+    # 送信者為Bot時無視
+    if message.author.bot:
+        return
+    await client.process_commands(message)
+    #私訊
+    if message.guild == None:
+        return
+    result = service.spreadsheets().values().get(
+    spreadsheetId=SpreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
+    if not values:
+        return
+    else:
+        for row in values:
+            if (message.channel.name == row[0] or row[0] == ''):
+                keywords = row[1].split()
+                check = True
+                for keyword in keywords:
+                    if not keyword in message.content:
+                        check = False
+                        break
+                if check:
+                    if message.author.nick == None:
+                        username = message.author.name
+                    else:
+                            username = message.author.nick
+                    if '<ban>' in row[2]:
+                        await message.author.ban()
+                    else:
+                        if '<kick>' in row[2]:
+                            await message.author.kick()
+                        if '<delete>' in row[2]:
+                            await message.delete()
+                        else:
+                            if '<reply>' in row[2]:
+                                await message.reply(row[3].replace('<username>',username))
+                            if '<replyrandom>' in row[2]:
+                                msgs = row[3].split('|')
+                                if len(msgs) != 0:
+                                    index = random.randint(0, len(msgs)-1)
+                                    await message.reply(msgs[index].replace('<username>',username))
+                    if '<send>' in row[2]:
+                        await message.channel.send(row[3].replace('<username>',username))
+                    if '<sendrandom>' in row[2]:
+                        msgs = row[3].split('|')
+                        if len(msgs) != 0:
+                            index = random.randint(0, len(msgs)-1)
+                            await message.channel.send(msgs[index].replace('<username>',username))
+                    return
+    
+# Bot起動
+client.run(TOKEN)
+
+# 起動時呼叫
+@client.event                             
+async def on_ready():                  
+    print(f'登入：{client.user}') 
+    discord.Activity(type=nextcord.ActivityType.playing, name="打/help領指令清單")
+        await client.change_presence(activity=activity_w)   
+        await asyncio.sleep(5)
+        activity_w = discord.Activity(type=nextcord.ActivityType.playing, name="正在服務YT陳宥輔")
+        await client.change_presence(activity=activity_w)
+        await asyncio.sleep(5)
 
 #添加身分組
 @client.event
